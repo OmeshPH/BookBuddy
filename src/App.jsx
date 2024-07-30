bc7af03766bd8f92b2b1035492a3813d30759bfd
OmeshPH <OmeshPH@gmail.com>
2024-07-30 01:20:39 -0400

    update app and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   
-    </>
-  )
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
-export default App
+export default App;   
+
